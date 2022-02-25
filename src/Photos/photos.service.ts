57e629308c341fb6ca7507c180c4289ebc1fa333
Juan Pablo Acosta <pabloacosta.b17@gmail.com>
2022-02-25 00:30:07 -0500

    search photos by title feature implemented and server error response implemented

--- src/Photos/photos.service.ts
+++ src/Photos/photos.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { PhotosDto } from './dto/photos.dto';
 import { PhotosRepository } from './repository/photos.repository';
@@ -16,11 +16,33 @@ export class PhotosService {
   }
 
   async getPhotoById(id: number): Promise<Photo> {
-    return this.photosRepository.findOne(id);
+    const photo = await this.photosRepository.findOne(id);
+    if (photo === undefined) {
+      throw new HttpException(
+        {
+          status: 404,
+          error:
+            "we are sorry, but the photo you're looking for does not exist.",
+        },
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    return photo;
   }
 
   async getPhotosByTitle(title: any): Promise<Photo[]> {
-    return this.photosRepository.getPhotosByTitle(title);
+    const photos = await this.photosRepository.getPhotosByTitle(title);
+    if (photos.length === 0) {
+      throw new HttpException(
+        {
+          status: 404,
+          error:
+            "we are sorry, but the title you're looking for does not exist.",
+        },
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    return photos;
   }
 
   async createPhoto(photosDto: PhotosDto): Promise<Photo> {
